96793c3b850710d395442a2c646c99461c787a67
davidmn <dave@davidmn.org>
2015-07-08 16:53:18 +0100

    added .gitignore, updated bibtex, added more data processing and plotting in prototype

--- src/prototype.py
+++ src/prototype.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np 
 import os
+import pylab as plt
 
 
 # The paths of the files used in the detection section
@@ -16,11 +17,12 @@ videoStream = cv2.VideoCapture(videoPath)
 # sample the video, taking a frame and downsampling for use in the detector
 ret,frame = videoStream.read()
 downsampledFrame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25) 
+frameSize = downsampledFrame.shape
 
 # produce a string that contains the paths to the PBD binary, model file and current frame then run PBD
 cv2.imwrite(framePath,downsampledFrame)
 command = detectorPath + ' ' + modelPath + ' ' + framePath
-os.system(command)
+#os.system(command)
 
 # open the data file and convert from floats to strings
 rawDataString = [line.rstrip('\n') for line in open(inFilePath)]
@@ -33,4 +35,23 @@ for element in rawDataString:
 
 	floatData.append(tempFloats)
 
+# convert to numpy array with two columns free for rectangle centres
+pose = np.zeros([len(floatData),6], dtype=float)
+for i in xrange(0,pose.shape[0]):
+	for j in xrange(0,pose.shape[1]-2):
+		pose[i,j] = floatData[i][j]
+	
 
+# calculate the centers and split into feature vector
+pose[:,4] = pose[:,0] + np.divide(pose[:,2], 2)
+pose[:,5] = pose[:,1] + np.divide(pose[:,3], 2)
+featureVector = pose[:,4:]
+
+#normalise the feature vector
+featureVector[:,0] = np.divide(featureVector[:,0],frameSize[1])
+featureVector[:,1] = np.divide(featureVector[:,1],frameSize[0])
+
+# show scatter
+plt.scatter(featureVector[:,0],featureVector[:,1])
+plt.axis([0,1,0,1])
+plt.show()
